e0d6c1b9e0c2062bd973b1c769ea3af47b20da3f
Jeongho Park / Joel <41176426+qkrjh0904@users.noreply.github.com>
2019-06-18 12:08:33 +0900

    [Add] 2D Context

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,21 +13,25 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-}
-
-function onMouseDown(event){
-    stopPainting();
-}
-function onMouseUp(event){
-    stopPainting();
+    if(!painting){  //마우스를 클릭 안할때 계속해서 현제 위치파악
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else{ //마우스를 클릭할때 이어줌
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", stopPainting;
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
